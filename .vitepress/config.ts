fd0183de6da1e6f7aa8484036aa6e5d0def222ef
Xyza Rivera <xyzarivera@gmail.com>
2026-06-17 11:39:31 +0900

    feat: update config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,23 +2,28 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "this time it's documented",
+  title: "documentation for one user",
   base: '/docs/',
   srcDir: './src',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Conventions', link: '/conventions/' }
     ],
 
     sidebar: [
       {
         text: 'Conventions',
         items: [
-          { text: 'Index', link: '/conventions/' },
+          { text: 'TL;DR', link: '/conventions/' },
           { text: 'TypeScript', link: '/conventions/typescript' },
           { text: 'Testing', link: '/conventions/testing' }
+        ]
+      },
+      {
+        text: 'Books',
+        items: [
+          { text: 'WIP', link: '/books/' },
         ]
       }
     ],
